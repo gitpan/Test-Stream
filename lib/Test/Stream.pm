1a85398908292006f90b0f154fe73274f7ea522d
Chad Granum <exodist7@gmail.com>
2014-08-29 05:53:07 +0000

    Import of EXODIST/Test-Stream-0.000001 from CPAN.
    gitpan-cpan-distribution: Test-Stream
    gitpan-cpan-version:      0.000001
    gitpan-cpan-path:         EXODIST/Test-Stream-0.000001.tar.gz
    gitpan-cpan-author:       EXODIST
    gitpan-cpan-maturity:     released

---
+++ lib/Test/Stream.pm
@@ -0,0 +1,84 @@
+package Test::Stream;
+use strict;
+use warnings;
+
+our $VERSION = '0.000001';
+
+sub exists { 1 }
+
+=pod
+
+=head1 NAME
+
+Test::Stream - Comming soon
+
+=head1 DESCRIPTION
+
+comming soom
+
+=cut
+
+package Test::Stream::ArrayBase;
+sub exists { 1 }
+
+package Test::Stream::Context;
+sub exists { 1 }
+
+package Test::Stream::Event::Bail;
+sub exists { 1 }
+
+package Test::Stream::Event::Child;
+sub exists { 1 }
+
+package Test::Stream::Event::Diag;
+sub exists { 1 }
+
+package Test::Stream::Event::Finish;
+sub exists { 1 }
+
+package Test::Stream::Event::Note;
+sub exists { 1 }
+
+package Test::Stream::Event::Ok;
+sub exists { 1 }
+
+package Test::Stream::Event::Plan;
+sub exists { 1 }
+
+package Test::Stream::Event;
+sub exists { 1 }
+
+package Test::Stream::ExitMagic;
+sub exists { 1 }
+
+package Test::Stream::Exporter;
+sub exists { 1 }
+
+package Test::Stream::Fork;
+sub exists { 1 }
+
+package Test::Stream::Formatter::TAP;
+sub exists { 1 }
+
+package Test::Stream::HashBase;
+sub exists { 1 }
+
+package Test::Stream::IOSets;
+sub exists { 1 }
+
+package Test::Stream::Listener::Legacy;
+sub exists { 1 }
+
+package Test::Stream::Provider;
+sub exists { 1 }
+
+package Test::Stream::Threads;
+sub exists { 1 }
+
+package Test::Stream::Tools;
+sub exists { 1 }
+
+package Test::Stream::Util;
+sub exists { 1 }
+
+1;
---
+++ t/sample.t
@@ -0,0 +1,7 @@
+use Test::More;
+use strict;
+use warnings;
+
+ok(1, "a test");
+
+done_testing;
