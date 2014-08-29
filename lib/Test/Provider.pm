f66ad9df99c221b47b70a719d37accb065d74582
Chad Granum <exodist7@gmail.com>
2014-08-29 18:52:44 +0000

    Import of EXODIST/Test-Stream-0.000002 from CPAN.
    gitpan-cpan-distribution: Test-Stream
    gitpan-cpan-version:      0.000002
    gitpan-cpan-path:         EXODIST/Test-Stream-0.000002.tar.gz
    gitpan-cpan-author:       EXODIST
    gitpan-cpan-maturity:     released

---
+++ lib/Test/Provider.pm
@@ -0,0 +1,33 @@
+package Test::Provider;
+use strict;
+use warnings;
+
+our $VERSION = '0.000002';
+
+sub exists { 1 }
+
+=pod
+
+=head1 NAME
+
+Test::Provider - Comming soon
+
+=head1 DESCRIPTION
+
+comming soom
+
+=cut
+
+package Test::Provider::Tools;
+sub exists { 1 }
+
+package Test::Provider::Context;
+sub exists { 1 }
+
+package Test::Provider::Utils;
+sub exists { 1 }
+
+package Test::Provider::Util;
+sub exists { 1 }
+
+1;
